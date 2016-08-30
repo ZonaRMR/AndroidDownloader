1e5f4b9308551534740989010cf85c3f19518fd0
严进一 <lexkde@aliyun.com>
2016-08-30 22:23:29 +0800

    Initial commit

---
+++ app/src/main/java/com/geekband/yanjinyi1987/downloader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.geekband.yanjinyi1987.downloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
